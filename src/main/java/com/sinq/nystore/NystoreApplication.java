b6693a94b16d323161a03b60978875478c184a79
yorane <yogesh.p.rane@capgemini.com>
2022-05-21 14:37:22 +0530

    master

---
+++ src/main/java/com/sinq/nystore/NystoreApplication.java
@@ -0,0 +1,13 @@
+package com.sinq.nystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NystoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NystoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sinq/nystore/NystoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinq.nystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NystoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
